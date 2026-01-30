518c21d6cec6df729c5ea56a66176a0ead2425dc
xxcosita3czxx <kokosove18@gmail.com>
2026-01-30 03:23:00 +0100

    eggs

--- bots/discord/langs/node/bot.js
+++ bots/discord/langs/node/bot.js
@@ -3,7 +3,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+    console.log(`Logged in as ${client.user.tag}`);// For started status this is required
 });
 
 client.login("insert token here");
